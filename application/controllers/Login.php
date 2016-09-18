6c058ccc981bac701c8932a1448ac3dcdd24ef68
M Imam Muttaqin <imutt20041998@gmail.com>
2016-09-18 14:35:27 +0700

    Update Login.php

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -28,20 +28,41 @@ class Login extends CI_Controller {
         $cek = $this->Model->Check_User($username,md5($password)); //melakukan persamaan data dengan database
         if(count($cek) == 1){ //cek data berdasarkan username & pass
             $DataUser = $this->Model->Get_User("where username = '$username'");
+            $username = $DataUser[0]['username'];
+            $userID = $DataUser[0]['userID'];
+            $sekolah = $DataUser[0]['sekolah'];
             $name = $DataUser[0]['name'];
             $status = $DataUser[0]['status'];
             $input = $DataUser[0]['input'];
             $this->session->set_userdata(array(
                 'isLogin'   => TRUE, //set data telah login
+                'username' => $DataUser[0]['username'],
                 'name' => $DataUser[0]['name'],
+                'sekolah' => $DataUser[0]['sekolah'],
+                'kota' => $DataUser[0]['kota'],
                 'userID' => $DataUser[0]['userID'],
                 'status' => $DataUser[0]['status'],
+                'input' => $DataUser[0]['input'],
             ));
             if($status == 0){
                 redirect('nilai');
             }else if($status == 1){
                 if($input == 1){
-                    //hayo sia naon 
+                    //Get Univ dan Nilai coeg
+                    $get_univ_1 = $this->Model->GetUniv("where userID = '$userID' and pilihan='1'");
+                    $get_univ_2 = $this->Model->GetUniv("where userID = '$userID' and pilihan='2'");
+                    $get_univ_3 = $this->Model->GetUniv("where userID = '$userID' and pilihan='3'");
+                    $ratanilaisemua = $DataUser[0]['ratanilaisemua'];
+
+                    $this->session->set_userdata(array(
+                        'ratanilaisemua'   => $ratanilaisemua,
+                        'univ_1' => $get_univ_1[0]['univ'],
+                        'univ_2' => $get_univ_2[0]['univ'],
+                        'univ_3' => $get_univ_3[0]['univ'],
+                        'jurusan_1' => $get_univ_1[0]['jurusan'],
+                        'jurusan_2' => $get_univ_2[0]['jurusan'],
+                        'jurusan_3' => $get_univ_3[0]['jurusan'],
+                    ));
                 }
                 redirect('dashboard');  //redirect ke halaman dashboard
             }
